cf2680d89775d0a5b13f67d10fd9dff63d170633
iMoe037 <i.mohamed037@gmail.com>
2015-12-14 18:11:49 -0500

    Create FactorialProgram.java

---
+++ FactorialProgram.java
@@ -0,0 +1,20 @@
+/**
+   This program demonstrates the Rectangle class's
+   constructor.
+*/
+
+public class FacotrialProgram
+{
+   public static void main(String[] args)
+   {
+	  int n =7;
+	  System.out.println(n + "! = " + BigIntegerFactorialPower.factorial(n));
+	  System.out.println(n + "^3 = " + BigIntegerFactorialPower.power(n,3));
+	  
+	  System.out.println(n + "! = " + BigIntegerFactorialPower.factorial(21));
+	  System.out.println("Google=" + BigIntegerFactorialPower.power(10,100));
+	  
+   }
+}
+	  
+	  
