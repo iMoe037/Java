8e2aefd956349beff694120a3396473ef422ad68
iMoe037 <i.mohamed037@gmail.com>
2015-12-14 18:32:50 -0500

    An attempt to make homework 2 less repetitive.

---
+++ Homework/homework5.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Homework5 {
+
+   public static void main(String[] args)
+   {
+	FillinBlank q1 = new FillinBlank("What is your name?","Immad");
+	String gans = JOptionPane.showInputDialog(q1.getQuestion());
+	JOptionPane.showMessageDialog(attemp
+	}
