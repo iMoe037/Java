1bc7db14a7e001329fa9a569c3d0212383b041f0
iMoe037 <i.mohamed037@gmail.com>
2015-12-14 18:08:36 -0500

    Create Bankcheckprogram.java

---
+++ Bankcheckprogram.java
@@ -0,0 +1,21 @@
+// This is a simple Java program.
+
+public class Bankcheckprogram
+
+{
+   public static void main(String[] args)
+   {	
+	int a= 0;
+	int b= 1;
+	int c= 3;
+	int d= 6;
+	int e= 0;
+	int f= 8;	
+	int g= 0;	
+	int h= 2;
+	int i= 0;
+	int t= 7*a + 3*b + c + 7*d + 3*e + f + 7*g +3*h + i;
+	int s= t%11;
+      System.out.println("BANK CHECK mod 10=" + s);
+   }
+}
