b8d8bf1912e6b163c02a92de44c1fef886d7ea4d
iMoe037 <i.mohamed037@gmail.com>
2015-12-14 18:36:21 -0500

    Used this to test the scoring function.

---
+++ Homework/Testing.java
@@ -0,0 +1,38 @@
+
+public static class Testing {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+{
+	int score;
+	public static boolean Turns; {
+	int i = 4;
+	if( attemp(String gans)){
+	 score = score + (i * 5);
+	i=0;
+	}
+else{
+	i>0;
+	i--;
+	
+}
+	
+	
+	
+	q1= newfillinblank (question, Answer);
+	System.out.println("Your Current Score is "score);
+	
+	FillinBlank q2 = new FillinBlank();
+	 System.out.println(q1.getans());
+	 q2.setquestion("Where are you from");
+	 q2.setans("NYC");
+	 if (q2.attempts()){
+	 }
+	 else
